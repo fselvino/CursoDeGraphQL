c530d593aeec91c3b3c099b532d8385e1a23dff6
Fernando <fernandoselvino@gmail.com>
2021-08-23 21:33:19 -0300

    Context - Evitando repetiçoes

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -1,13 +1,13 @@
-const user = async (_, { id }, { fetch }) => {
-  const response = await fetch('http://localhost:3000/users/' + id);
+const user = async (_, { id }, { getUsers }) => {
+  const response = await getUsers('/' + id);
   const user = await response.json();
-  console.log(user);
+
   return user;
 };
 
 //O fetch esta vindo do contexto de index.js
-const users = async (_, __, { fetch }) => {
-  const users = await fetch('http://localhost:3000/users');
+const users = async (_, __, { getUsers }) => {
+  const users = await getUsers();
   return users.json();
 };
 
